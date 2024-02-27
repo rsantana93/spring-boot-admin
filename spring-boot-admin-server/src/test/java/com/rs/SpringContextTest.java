079606b9f0a483e46c4d9944bc65117c57493076
rsantana93 <rikardinhosousa98@gmail.com>
2024-02-27 17:52:50 -0300

    feat: projeto de estudos spring boot admin

---
+++ spring-boot-admin-server/src/test/java/com/rs/SpringContextTest.java
@@ -0,0 +1,17 @@
+package com.rs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.rs.springbootadminserver.SpringBootAdminServerApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootAdminServerApplication.class)
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
